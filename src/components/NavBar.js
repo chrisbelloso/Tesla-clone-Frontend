7857ba57cc04afae9f96b97ebbf07f56c52a4b39
chrisbelloso <christianbelloso2002@gmail.com>
2022-03-27 15:07:17 -0400

    Initial commit (NavBar)

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import { Navbar, Container, Nav, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div>
+          <Navbar fixed='top' style={{backgroundColor: "rgb(37, 37, 37)"}} expand={false}>
+              <Container fluid>
+                <Navbar.Brand className='NavTitle'>
+                  <Link to="/">
+                    TESLA
+                  </Link>
+                </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                    <Navbar.Offcanvas
+                      id="offcanvasNavbar"
+                      aria-labelledby="offcanvasNavbarLabel"
+                      placement="end"
+                    >
+                      <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">EXPLORE</Offcanvas.Title>
+                      </Offcanvas.Header>
+                      <Offcanvas.Body>
+                        <Nav className='list'>
+                          <Link to="/">Home</Link>
+                          <Link to="/links">Important Links</Link>
+                        </Nav>
+                      </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+          </Navbar>
+        </div>
+    )
+}
+
+export default NavBar;
+
+
---
+++ src/views/MainView.js
@@ -0,0 +1,11 @@
+const MainView = () => {
+    return (
+        <>
+        <div>
+            <h2>Main View</h2>
+        </div>
+        </>
+    )
+}
+
+export default MainView;
