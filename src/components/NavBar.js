6bfad2db343a59f285f58d9a9c6d7b563756d9bf
chrisbelloso <christianbelloso2002@gmail.com>
2022-03-28 23:03:09 -0400

    Mostly CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,31 +6,20 @@ import './NavBar.css';
 const NavBar = () => {
     return (
         <div>
-          <Navbar fixed='top' style={{backgroundColor: "rgb(37, 37, 37)"}} expand={false}>
-              <Container fluid>
-                <Navbar.Brand className='NavTitle'>
-                  <Link to="/">
-                    TESLA
-                  </Link>
-                </Navbar.Brand>
-                  <Navbar.Toggle aria-controls="offcanvasNavbar" />
-                    <Navbar.Offcanvas
-                      id="offcanvasNavbar"
-                      aria-labelledby="offcanvasNavbarLabel"
-                      placement="end"
-                    >
-                      <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel">EXPLORE</Offcanvas.Title>
-                      </Offcanvas.Header>
-                      <Offcanvas.Body>
-                        <Nav className='list'>
-                          <Link to="/">Home</Link>
-                          <Link to="/links">Important Links</Link>
-                        </Nav>
-                      </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-          </Navbar>
+          <Navbar bg="transparent" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">TESLA</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#ModelSDetails">Model S</Nav.Link>
+        <Nav.Link href="#Model3Details">Model 3</Nav.Link>
+        <Nav.Link href="#ModelXDetails">Model X</Nav.Link>
+        <Nav.Link href="#ModelYDetails">Model Y</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
         </div>
     )
 }
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,8 +1,25 @@
 const MainView = () => {
     return (
         <>
-        <div>
-            <h2>Main View</h2>
+        <div style={{
+            backgroundImage: `url("https://tesla-cdn.thron.com/delivery/public/image/tesla/3863f3e5-546a-4b22-bcbc-1f8ee0479744/bvlatuR/std/1200x628/MX-Social")`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            height: "800px"
+        }}>
+            <h2>
+                Model X
+            </h2>
+        </div>
+        <div style={{
+            backgroundImage: `url("https://cdn.motor1.com/images/mgl/P124r/s1/tesla-model-s.webp")`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            height: "800px"
+        }}>
+            <h2>
+                Model X
+            </h2>
         </div>
         </>
     )
